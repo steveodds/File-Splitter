160e19bb1367d6d958c7199c57526a6406fba76c
Stephen Odundo <steveremdi@gmail.com>
2020-01-15 19:29:08 +0300

    Changed folder types and locations

--- P2P File Sharing/StartupFunctions.cs
+++ P2P File Sharing/StartupFunctions.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Data.SQLite;
 using System.Threading;
 using System.Windows.Threading;
+using static P2P_File_Sharing.StatusMessage;
 
 namespace P2P_File_Sharing
 {
@@ -11,34 +12,63 @@ namespace P2P_File_Sharing
     {
         public class StartupFunctions
         {
-            private static readonly string PEER_STORAGE_FOLDER = String.Concat(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "\\Peer_Storage");
-            private static readonly string zippedTempF = String.Concat(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "\\Peer_Storage\\ZippedTemp");
-            private static readonly string clusteredTemp = String.Concat(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "\\Peer_Storage\\ClusteredTemp");
-            private static readonly string storageF = String.Concat(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "\\Peer_Storage\\Storage");
+            private static readonly string _storageFolder = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\EFM-Storage";
+            private static readonly string _encrypted = $@"{_storageFolder}\Encrypted";
+            private static readonly string _compressed = $@"{_encrypted}\Compressed";
+            private static readonly string _dbFile = $@"{Directory.GetCurrentDirectory()}\ds.sqlite";
+            public string Encrypted
+            {
+                get
+                {
+                    return _encrypted;
+                }
+            }
+
+            public string Compressed
+            {
+                get
+                {
+                    return _compressed;
+                }
+            }
 
             public static void DirectoryCheck()
             {
-                if (!Directory.Exists(PEER_STORAGE_FOLDER))
+                if (!Directory.Exists(_storageFolder))
                 {
-                    Directory.CreateDirectory(PEER_STORAGE_FOLDER);
-                    if (!Directory.Exists(zippedTempF) && !Directory.Exists(clusteredTemp) && !Directory.Exists(storageF))
-                    {
-                        Directory.CreateDirectory(zippedTempF);
-                        Directory.CreateDirectory(clusteredTemp);
-                        Directory.CreateDirectory(storageF);
-                        //TODO: Update status message to reflect these changes.
-                    }
+                    Directory.CreateDirectory(_storageFolder);
+                    Directory.CreateDirectory(_encrypted);
+                    Directory.CreateDirectory(_compressed);
+                    //TODO: Update status message to reflect these changes.
+                    PostToActivityBox("Created storage folders.", MessageType.INFORMATION);
                 }
+
                 //TODO Send "Ready" message to status message.
+                if (!Directory.Exists(_encrypted))
+                {
+                    Directory.CreateDirectory(_encrypted);
+                    Directory.CreateDirectory(_compressed);
+                    PostToActivityBox("Created missing folders.", MessageType.INFORMATION);
+                }
+
+                if (!Directory.Exists(_compressed))
+                {
+                    Directory.CreateDirectory(_compressed);
+                    PostToActivityBox("Created missing folders.", MessageType.INFORMATION);
+                }
             }
 
-            public static void DBCheck()
+            public static bool IsDBFilePresent()
             {
-                if (!File.Exists(String.Concat(Directory.GetCurrentDirectory(), "\\ds.sqlite")))
+                return File.Exists(_dbFile);
+            }
+            public static void DBCreate()
+            {
+                if (!IsDBFilePresent())
                 {
                     SQLiteConnection.CreateFile("ds.sqlite");
                     DBBuilder();
-                    DBCheck();
+                    DBCreate();
                 }
                 else
                 {
@@ -47,6 +77,7 @@ namespace P2P_File_Sharing
                     SQLiteConnection dsConTableCheck = new SQLiteConnection("Data Source=ds.sqlite;Version=3;");
                     dsConTableCheck.Open();
 
+                    //TODO: Change information type stored in database
                     using (SQLiteCommand sQLiteCommand = new SQLiteCommand(dsConTableCheck))
                     {
                         sQLiteCommand.CommandText = "select count(*) from sqlite_master where type = 'table' and name = 'firstHash';";
@@ -69,7 +100,7 @@ namespace P2P_File_Sharing
                     if (tablecheck.Sum() != 5)
                     {
                         DBBuilder();
-                        DBCheck();
+                        DBCreate();
                     }
                 }
             }
@@ -78,14 +109,17 @@ namespace P2P_File_Sharing
             {
                 SQLiteConnection dsCon = new SQLiteConnection("Data Source=ds.sqlite;Version=3;");
                 dsCon.Open();
+                //TODO: Change database structure
                 string createHashTable = "create table if not exists firstHash(filename varchar(256), hash varchar(50));";
                 string createEncTable = "create table if not exists secondHash(filename varchar(256), encHash varchar(50));";
                 string peerTable = "create table if not exists peerList(peerName varchar(50), peerLoc varchar(50));";
                 string peerFiles = "create table if not exists dsFileLoc(clusterID varchar(50), peerLoc varchar(50));";
                 string clusterCheck = "create table if not exists fileclusters(clusterID varchar(50), chunksNumber int);";
 
-                SQLiteCommand creationCommands = new SQLiteCommand();
-                creationCommands.Connection = dsCon;
+                SQLiteCommand creationCommands = new SQLiteCommand
+                {
+                    Connection = dsCon,
+                };
                 creationCommands.CommandText = createHashTable;
                 creationCommands.CommandText += createEncTable;
                 creationCommands.CommandText += peerTable;
