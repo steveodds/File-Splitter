12587feca20e14dfab013a52e60ad746615790a3
Stephen Odundo <steveremdi@gmail.com>
2020-01-17 22:42:39 +0300

    Created base template for generating file hashes

---
+++ P2P File Sharing/FileHash.cs
@@ -0,0 +1,29 @@
+﻿namespace P2P_File_Sharing
+{
+    public class FileHash
+    {
+        private string _fileName;
+        private string _hash;
+        private string _formattedHash;
+        public FileHash(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public string GenerateFileHash()
+        {
+            //TODO Generate hash of file
+
+            return _hash;
+        }
+
+        public string FormatFileHash()
+        {
+            if (string.IsNullOrEmpty(_hash))
+                GenerateFileHash();
+
+            //TODO Format hash string
+            return _formattedHash;
+        }
+    }
+}
