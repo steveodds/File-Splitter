b60ea12289369463879fa175ad5b7f446526dcc4
Stephen Odundo <steveremdi@gmail.com>
2020-01-19 22:54:21 +0300

    Added base class for file encryption

---
+++ P2P File Sharing/FileEncryptor.cs
@@ -0,0 +1,36 @@
+﻿namespace P2P_File_Sharing
+{
+    public class FileEncryptor
+    {
+        private readonly string _fileName;
+        private readonly string _encryptedHash;
+        public FileEncryptor(string filename)
+        {
+            _fileName = filename;
+        }
+
+        public void Encrypt()
+        {
+            //TODO encrypt file
+        }
+
+        public bool IsAlreadyEncrypted()
+        {
+            //TODO check state in db
+
+            return false;
+        }
+
+        public void Decrypt()
+        {
+            //TODO decrypt file
+        }
+
+        public bool IsAlreadyDecrypted()
+        {
+            //TODO check state in db
+
+            return false;
+        }
+    }
+}
